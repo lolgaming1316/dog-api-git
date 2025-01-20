5c9e955b15f93460bad7b08be43e1b445685f06e
lolgaming1316 <159759022+lolgaming1316@users.noreply.github.com>
2025-01-20 08:24:18 +0100

    Update vite.config.js

--- dog-api/vite.config.js
+++ dog-api/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/dog-api-git/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
